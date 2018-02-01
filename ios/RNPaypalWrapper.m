27832fad618ac5caac6560c15a4463be4487792b
Yao Bin <alvinthen890703@gmail.com>
2018-02-01 12:57:46 +0800

    Accept credit card

--- ios/RNPaypalWrapper.m
+++ ios/RNPaypalWrapper.m
@@ -106,7 +106,7 @@ RCT_EXPORT_METHOD(pay:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)r
     [self.payment setShortDescription:description];
     
     self.configuration = [[PayPalConfiguration alloc] init];
-    [self.configuration setAcceptCreditCards:false];
+    [self.configuration setAcceptCreditCards:true];
     [self.configuration setPayPalShippingAddressOption:PayPalShippingAddressOptionPayPal];
     
     PayPalPaymentViewController *vc = [[PayPalPaymentViewController alloc] initWithPayment:self.payment
